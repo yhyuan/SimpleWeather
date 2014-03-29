ea61b6849f9430c155c29335a15e130158245d46
Yinhuan Yuan <yhyuan@gmail.com>
2014-03-28 20:07:35 -0400

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by Yuan Yinhuan on 14-3-28.
+//  Copyright (c) 2014年 Yuan Yinhuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Yuan Yinhuan on 14-3-28.
+//  Copyright (c) 2014年 Yuan Yinhuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
